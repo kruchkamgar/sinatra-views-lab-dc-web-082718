de7ec309022c8a9e10efd5a1e3287f911893463e
kruchkamgar <kruchkamgar@gmail.com>
2018-09-17 16:07:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,28 @@
+require 'date'
+require 'pry'
+
+
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		date = DateTime.now
+		# @date = "#{date.day}, #{date.month} #{date.day}, #{date.year}."
+		@date = date.strftime('%A, %B %-d, %Y')
+		binding.pry
+		erb :date
+	end
 
 end
